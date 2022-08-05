34cb6c82b79e49cf3f69884fb40828249b6975a7
ysh12274856@gmail.com <ysh12274856@gmail.com>
2022-08-05 16:02:37 +0900

    👍DatePicker 도전문제

--- Picker_04/ViewController.swift
+++ Picker_04/ViewController.swift
@@ -9,9 +9,10 @@ import UIKit
 
 class ViewController: UIViewController {
     let timeSelector : Selector = #selector(ViewController.updateTime)
+    var currentTime : String? ,choiceTime : String?
     let interval = 1.0
-    var count = 0
-
+    
+    
     @IBOutlet var Ib1CurrentTime: UILabel!
     @IBOutlet var Ib1PickerTime: UILabel!
     
@@ -29,8 +30,14 @@ class ViewController: UIViewController {
         //현재 시간을 NSDate 함수를 사용하여 가져오기
             
         let formatter = DateFormatter() //날짜를 출력할 수 있도록 하는 클래스선언
-        formatter.dateFormat = "yyyy-MM-dd HH:mm EEE"
-        Ib1CurrentTime.text = "현재시간: " + formatter.string(from: date as Date)
+        formatter.dateFormat = "yyyy-MM-dd HH:mm"
+        currentTime = formatter.string(from: date as Date)
+        print(currentTime)
+        if (currentTime == choiceTime){
+            view.backgroundColor = UIColor.red
+        }else{
+            view.backgroundColor = UIColor.white
+        }
     }
     
     
@@ -38,8 +45,10 @@ class ViewController: UIViewController {
         let datePickerView = sender
         
         let formatter = DateFormatter()
-        formatter.dateFormat = "yyyy-MM-dd HH:mm EEE"
-        Ib1PickerTime.text = "선택시간: " + formatter.string(from: datePickerView.date)
+        formatter.dateFormat = "yyyy-MM-dd HH:mm"
+        choiceTime = formatter.string(from: datePickerView.date)
+        print(choiceTime)
+
     }
     
 }
