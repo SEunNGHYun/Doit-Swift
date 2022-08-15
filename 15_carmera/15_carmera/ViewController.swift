c5b22aebcb899e6992d6a91d5e8ab0422fc63555
ysh12274856@gmail.com <ysh12274856@gmail.com>
2022-08-15 16:59:00 +0900

    16_DrawGrapics

--- 15_carmera/15_carmera/ViewController.swift
+++ 15_carmera/15_carmera/ViewController.swift
@@ -6,14 +6,132 @@
 //
 
 import UIKit
+import MobileCoreServices
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UINavigationControllerDelegate, UIImagePickerControllerDelegate{
+    
+    let imagePicker : UIImagePickerController! = UIImagePickerController()
+    var captureImage : UIImage!
+    var videoURL: URL!
+    var flagImageSave = false
+    var numImage = 0
+    
+    @IBOutlet var imageView3: UIImageView!
+    @IBOutlet var ImageView2: UIImageView!
+    @IBOutlet var imageView: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    
+    @IBAction func btnCaptureImageFromCarnera(_ sender: UIButton) {
+        if(UIImagePickerController.isSourceTypeAvailable(.camera)){
+            flagImageSave = true
+            
+            numImage = numImage + 1
+            
+            imagePicker.delegate = self
+            imagePicker.sourceType = .camera
+            imagePicker.mediaTypes = ["public.image"]
+            imagePicker.allowsEditing = false
+            
+            present(imagePicker, animated: true, completion: nil)
+        }else{
+            myAlert("Camera inaccessable", message: "application cannot access the camera")
+        }
+    }
+    @IBAction func btnLoadImageFromLibary(_ sender: UIButton) {
+        if(UIImagePickerController.isSourceTypeAvailable(.photoLibrary)){
+            flagImageSave = false
+            
+            numImage = numImage + 1
+            
+            imagePicker.delegate = self
+            imagePicker.sourceType = .photoLibrary
+            imagePicker.mediaTypes = ["public.image"]
+            imagePicker.allowsEditing = true
+            
+            present(imagePicker, animated:true)
+        }else{
+            myAlert("photo album inaccessable", message: "application cannot access the photo album")
+        }
+    }
+    
+    @IBAction func btnRecoardVideoFromCarmera(_ sender: UIButton) {
+        if(UIImagePickerController.isSourceTypeAvailable(.camera)){
+            flagImageSave = true
+            
+            imagePicker.delegate = self
+            imagePicker.sourceType = .camera
+            imagePicker.mediaTypes = ["public.movie"]
+            imagePicker.allowsEditing = false
+            
+            present(imagePicker, animated: true, completion: nil)
+        }else{
+            myAlert("Camera inaccessable", message: "application cannot access the camera")
+        }
+        
+    }
+    
+    @IBAction func btnLoadVideoFromLibary(_ sender: UIButton) {
+        if(UIImagePickerController.isSourceTypeAvailable(.photoLibrary)){
+            flagImageSave = false
+            
+            imagePicker.delegate = self
+            imagePicker.sourceType = .photoLibrary
+            imagePicker.mediaTypes = ["public.movie"]
+            imagePicker.allowsEditing = false
+            
+            present(imagePicker, animated:true)
+        }else{
+            myAlert("photo album inaccessable", message: "application cannot access the photo album")
+        }
+    }
+    
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        numImage = numImage - 1
+        self.dismiss(animated: true, completion: nil)
+    }
+    
+    @IBAction func btnEmpty(_ sender: Any) {
+        imageView3.image = nil
+        ImageView2.image = nil
+        imageView.image = nil
+    }
+    
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        let mediaType = info[UIImagePickerController.InfoKey.mediaType] as! NSString
+        
+        if mediaType.isEqual(to: "public.image" as String){
+            captureImage = info[UIImagePickerController.InfoKey.originalImage] as! UIImage
+            
+            if flagImageSave {
+                UIImageWriteToSavedPhotosAlbum(captureImage, self, nil, nil)
+            }
+            
+            if numImage == 0{
+                imageView.image = captureImage
+            }else if numImage == 1 {
+                ImageView2.image = captureImage
+            }else{
+                imageView3.image = captureImage
+            }
+        }else {
+            if flagImageSave {
+                videoURL = (info[UIImagePickerController.InfoKey.mediaURL] as! URL)
+            }
+            UISaveVideoAtPathToSavedPhotosAlbum(videoURL.relativePath, self, nil, nil)
+        }
+    }
+    
+    func myAlert(_ title : String, message : String){
+        let alert = UIAlertController(title: title, message: message, preferredStyle: UIAlertController.Style.alert)
+        let action  = UIAlertAction(title: "ok", style: UIAlertAction.Style.default, handler: nil)
+        
+        alert.addAction(action)
+        self.present(alert, animated: true, completion: nil)
+    }
 }
 
---
+++ 16_DrawGraphics/16_DrawGraphics/ViewController.swift
@@ -0,0 +1,138 @@
+//
+//  ViewController.swift
+//  16_DrawGraphics
+//
+//  Created by 윤승현 on 2022/08/15.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet var imgView: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func btnDrawLine(_ sender: UIButton) {
+        UIGraphicsBeginImageContext(imgView.frame.size)
+        let context = UIGraphicsGetCurrentContext()!
+        
+        context.setLineWidth(2.0)
+        context.setStrokeColor(UIColor.red.cgColor)
+        
+        context.move(to: CGPoint(x: 70, y: 50))
+        context.addLine(to: CGPoint(x: 270, y: 250))
+        
+        context.strokePath()
+        
+        context.setLineWidth(4.0)
+        context.setStrokeColor(UIColor.blue.cgColor)
+        
+        context.move(to: CGPoint(x: 170, y: 200))
+        context.addLine(to: CGPoint(x: 270, y: 350))
+        context.addLine(to: CGPoint(x: 70, y: 350))
+        context.addLine(to: CGPoint(x: 170, y: 200))
+        context.strokePath()
+        
+        imgView.image = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+    }
+    
+    @IBAction func btnDrawRectangle(_ sender: UIButton) {
+        UIGraphicsBeginImageContext(imgView.frame.size)
+        let context = UIGraphicsGetCurrentContext()!
+        
+        context.setLineWidth(2.0)
+        context.setStrokeColor(UIColor.red.cgColor)
+        
+        context.addRect(CGRect(x: 70, y: 100, width: 200, height: 200))
+        context.strokePath()
+        
+        imgView.image = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+    }
+    
+    @IBAction func btnDrawCircle(_ sender: UIButton) {
+        UIGraphicsBeginImageContext(imgView.frame.size)
+        let context = UIGraphicsGetCurrentContext()!
+        
+        context.setLineWidth(2.0)
+        context.setStrokeColor(UIColor.red.cgColor)
+        
+        context.addEllipse(in: CGRect(x: 70, y: 50, width: 200, height: 100))
+        context.strokePath()
+        // 선 굵기를 다르게 해서 컨텍스트에 적용하기에 strokePath 두번 추가
+        context.setLineWidth(5.0)
+        context.setStrokeColor(UIColor.green.cgColor)
+        
+        context.addEllipse(in: CGRect(x: 70, y: 200, width: 200, height: 200))
+        context.strokePath()
+        
+        imgView.image = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+    }
+    @IBAction func btnDrawArc(_ sender: UIButton) {
+        //그리기 시작
+        UIGraphicsBeginImageContext(imgView.frame.size)
+        //그리는 화면 사이즈를 이미지뷰 화면 사이즈에 맞게 설정
+        let context = UIGraphicsGetCurrentContext()!
+        //설정에 맞게 컨텍스트 생성
+        
+        context.setLineWidth(5.0)
+        context.setStrokeColor(UIColor.red.cgColor)
+        
+        context.move(to: CGPoint(x: 100, y: 50))
+        context.addArc(tangent1End: CGPoint(x: 250, y: 50), tangent2End: CGPoint(x: 250, y: 200), radius: CGFloat(50))
+        context.addLine(to: CGPoint(x: 250, y: 200))
+        
+        context.move(to: CGPoint(x: 100, y: 250))
+        context.addArc(tangent1End: CGPoint(x: 270, y: 250), tangent2End: CGPoint(x: 100, y: 400), radius: CGFloat(20))
+        context.addLine(to: CGPoint(x: 100, y: 400))
+
+        context.strokePath()
+        //그려진 선들을 컨텍스트에 적용
+        
+        imgView.image = UIGraphicsGetImageFromCurrentImageContext()
+        //위에 그린거를 이미지로 변환하여 imageView에 넣어 화면 출력
+        UIGraphicsEndImageContext()
+        //그리기 끝
+    }
+    
+    @IBAction func btnDrawFill(_ sender: UIButton) {
+        UIGraphicsBeginImageContext(imgView.frame.size)
+        let context = UIGraphicsGetCurrentContext()!
+        
+        context.setLineWidth(1.0)
+        context.setStrokeColor(UIColor.red.cgColor)
+        context.setFillColor(UIColor.red.cgColor)
+        let rectangle = CGRect(x: 70, y: 50, width: 200, height: 100)
+        context.addRect(rectangle)
+        context.fill(rectangle)
+        context.strokePath()
+        
+        context.setLineWidth(1.0)
+        context.setStrokeColor(UIColor.red.cgColor)
+        context.setFillColor(UIColor.blue.cgColor)
+        let circle = CGRect(x: 70, y: 200, width: 200, height: 100)
+        context.addEllipse(in: circle)
+        context.fillEllipse(in: circle)
+        context.strokePath()
+        
+        context.setLineWidth(2.0)
+        context.setStrokeColor(UIColor.red.cgColor)
+        context.setFillColor(UIColor.red.cgColor)
+        context.move(to: CGPoint(x: 170, y: 350))
+        context.addLine(to: CGPoint(x: 270, y: 450))
+        context.addLine(to: CGPoint(x: 70, y: 450))
+        context.addLine(to: CGPoint(x: 170, y: 350))
+        context.fillPath()
+        context.strokePath()
+        
+        imgView.image = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+    }
+}
+
